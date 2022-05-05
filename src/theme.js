f43b485e7b421ef43175adcd2d10ebe5c217c830
Juan Manuel Roig Courtis <jmroigcourtis.dev@gmail.com>
2022-05-05 12:46:27 -0300

    Primera subida backup

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme, theme } from "@chakra-ui/react"
+
+export default extendTheme({
+    colors: {
+        whatsapp: theme.colors.whatsapp,
+        black: theme.colors.black,
+    },
+    
+})
+
