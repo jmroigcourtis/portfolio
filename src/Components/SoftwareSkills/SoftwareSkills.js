a114f58a72449fc779ffb53e22315edfcdf39952
Juan Manuel Roig Courtis <jmroigcourtis.dev@gmail.com>
2022-05-06 00:05:33 -0300

    Subida backup

--- src/Components/SoftwareSkills/SoftwareSkills.js
+++ src/Components/SoftwareSkills/SoftwareSkills.js
@@ -8,7 +8,7 @@ export const SoftwareSkills = () => {
     return  (
         <Box w={'50%'} m={'auto'} pos={'relative'}>
                            <HStack justifyContent={'center'} mt={5}>
-                           <Box bgColor={'blue.700'} w={60} shadow='base' position={'relative'} top={'50px'} zIndex={'2'} borderRadius={'50px'}>
+                           <Box bgColor={'blue.700'} w={60} shadow='lg' position={'relative'} top={'55px'} zIndex={'2'} borderRadius={'50px'}>
                                     <Text textAlign={'center'} fontSize={20} color={'blue.50'} fontWeight='600' p={2}>
                                        Lenguages & Tools
                                     </Text>
