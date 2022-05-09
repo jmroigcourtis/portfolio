5f61238af788adb52bfa92b0386c5e6c15255b7b
Juan Manuel Roig Courtis <jmroigcourtis.dev@gmail.com>
2022-05-09 09:54:25 -0300

    Subida

--- src/Components/Proyects/Proyects.js
+++ src/Components/Proyects/Proyects.js
@@ -15,19 +15,19 @@ export const Proyects = () => {
 
             <Flex justifyContent={'center'} mt={'100px'} p={10}>
                 <HStack spacing={'200px'}>
-                    <Box className="projectBox" cursor={'pointer'} borderRadius='lg' bgColor={'red.500'} borderWidth='2px' maxW='sm'  w={'400px'} h={'400px'} shadow='sm'>
-                        <BiJoystickAlt className="joystickIcon" />
                         <Link className="textStyle" borderRadius='lg' borderWidth={0}  _focus={'none'} href="https://geekland.vercel.app/" isExternal fontSize={'40px'} fontWeight={"bold"} textAlign={'center'} color={'white'} >
-                            <Text fontSize={'40px'} fontWeight={"bold"} textAlign={'center'} color={'white'}>Geekland</Text>
+                            <Box className="projectBox" cursor={'pointer'} borderRadius='lg' bgColor={'red.500'} borderWidth='2px' maxW='sm'  w={'400px'} h={'400px'} shadow='sm'>
+                                <BiJoystickAlt className="joystickIcon" />
+                                <Text fontSize={'40px'} fontWeight={"bold"} textAlign={'center'} color={'white'}>Geekland</Text>
+                            </Box>
                         </Link>
-                    </Box>
 
-                    <Box className="projectBox" cursor={'pointer'} borderRadius='lg' borderWidth='2px' maxW='sm' bgColor={'#14171e'}  w={'400px'} h={'400px'} shadow='sm'>
-                        <Image borderRadius='lg' h={'350px'} w={'500px'}src={RickMorty} alt='Rick and Morty'/>
                         <Link className="textStyle" borderRadius='lg' borderWidth={0}  _focus={'none'} href="https://mortyapp.vercel.app/" isExternal fontSize={'40px'} fontWeight={"bold"} textAlign={'center'} color={'white'} >
-                            <Text fontSize={'35px'} fontWeight={"bold"} textAlign={'center'} color={'white'} marginTop={'-50px'} >Rick and Morty app</Text>
+                            <Box className="projectBox" cursor={'pointer'} borderRadius='lg' borderWidth='2px' maxW='sm' bgColor={'#14171e'}  w={'400px'} h={'400px'} shadow='sm'>
+                                <Image borderRadius='lg' h={'350px'} w={'500px'}src={RickMorty} alt='Rick and Morty'/>
+                                <Text fontSize={'35px'} fontWeight={"bold"} textAlign={'center'} color={'white'} marginTop={'-50px'} >Rick and Morty app</Text>
+                            </Box>
                         </Link>
-                    </Box>
 
                     <Box borderRadius='lg' borderWidth='2px' maxW='sm'  w={'400px'} h={'400px'} shadow='sm'>
                     
