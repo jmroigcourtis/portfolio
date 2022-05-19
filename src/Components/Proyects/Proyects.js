ce625bf875d85f3554d5357de828b7aaf13d0ebf
Juan Manuel Roig Courtis <jmroigcourtis.dev@gmail.com>
2022-05-19 09:12:01 -0300

    Proyecto agregado

--- src/Components/Proyects/Proyects.js
+++ src/Components/Proyects/Proyects.js
@@ -1,6 +1,8 @@
 import { Box, Flex, HStack, Stack, Text, Image, Link} from "@chakra-ui/react"
 import {BiJoystickAlt} from 'react-icons/bi'
 import RickMorty from '../Images/rick.png'
+import ToDoList from '../Images/todo.jpg'
+import Logo from '../Images/etre.png'
 import './Proyects.css'
 
 export const Proyects = () => {
@@ -29,9 +31,12 @@ export const Proyects = () => {
                             </Box>
                         </Link>
 
-                    <Box borderRadius='lg' borderWidth='2px' maxW='sm'  w={'400px'} h={'400px'} shadow='sm'>
-                    
-                    </Box>
+                        <Link className="textStyle" borderRadius='lg' borderWidth={0}  _focus={'none'} href="https://jmroigcourtis.github.io/etrecounseling/" isExternal fontSize={'40px'} fontWeight={"bold"} textAlign={'center'} color={'white'} >
+                            <Box className="projectBox" borderRadius='lg' borderWidth='2px' maxW='sm'  w={'400px'} h={'400px'} shadow='sm' bgColor={'#faeee0'}>
+                            <Image borderRadius='lg' h={'350px'} src={Logo} alt='To do list app'/>
+                                    <Text fontSize={'35px'} fontWeight={"bold"} textAlign={'center'} color={'#f38da1'} marginTop={'-55px'} >Static landing page</Text>
+                            </Box>
+                        </Link>
                 </HStack>
             </Flex>
         </Stack>
